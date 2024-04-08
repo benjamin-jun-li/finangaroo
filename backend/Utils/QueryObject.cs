91d2349414259b09e48793c5cc89d21139c46da6
Benjamin Li <benjaminli060@gmail.com>
2024-04-08 21:29:57 +1000

    get stocks page size & added jwt

--- backend/Utils/QueryObject.cs
+++ backend/Utils/QueryObject.cs
@@ -9,5 +9,9 @@ namespace backend.Utils
     {
         public string? Symbol { get; set; } = null;
         public string? CompanyName { get; set;} = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDecending { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
     }
 }
